ae04a9eb1498b187b647166c5499b85a7bfc1c54
noualguill <noualguill@eisti.eu>
2021-02-05 09:44:15 +0100

    Css change

--- assets/js/hover.zoom.conf.js
+++ assets/js/hover.zoom.conf.js
@@ -7,7 +7,7 @@
             });
             
             $('.green').hoverZoom({
-                overlayColor: 'rgba(255, 255, 255, 0.5)',
+                overlayColor: '#1abc9c',
                 zoom: 0
             });
             
